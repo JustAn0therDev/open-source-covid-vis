0df138a75f84ba5ed22186ab15620947907b4e8a
Ruan Montelo <ruanmontelo@gmail.com>
2021-02-07 21:53:46 -0300

    Fixing news cache

--- pages/api/news.js
+++ pages/api/news.js
@@ -2,23 +2,24 @@ import fetch from 'node-fetch'
 import cache from 'memory-cache'
 
 export default async function getNews(req, res) {
-    const twoHoursInMilisseconds = 2 * 60 * 60 * 1000
     let cachedResponse = cache.get('response-news')
-    let response = await fetch('https://coronavirus-smartable.p.rapidapi.com/news/v1/US/', {
-            headers: {
-                "x-rapidapi-key": process.env.COVID_19_NEWS_API_KEY,
-                "x-rapidapi-host": "coronavirus-smartable.p.rapidapi.com",
-                "accept": "application/json"
-            },
-            method: 'GET'
-        })
-
     if (!cachedResponse) {
+        const response = await fetch('https://coronavirus-smartable.p.rapidapi.com/news/v1/US/', {
+                headers: {
+                    "x-rapidapi-key": process.env.COVID_19_NEWS_API_KEY,
+                    "x-rapidapi-host": "coronavirus-smartable.p.rapidapi.com",
+                    "accept": "application/json"
+                },
+                method: 'GET'
+            })
+        
+        const twoHoursInMilisseconds = 2 * 60 * 60 * 1000
         cache.put('response-news', await response.json(), twoHoursInMilisseconds, function (key, value) {
             console.log(`Cache expired. Re-building it. Key: ${key}. Value: ${value}`)
         })
-        cachedResponse = cache.get('response-news')
-    }     
+
+        cachedResponse = await response.json()
+    }
 
     return res.status(200).json(cachedResponse) 
 }
